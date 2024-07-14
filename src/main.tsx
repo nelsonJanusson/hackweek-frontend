2ec7520027224d904930f8353bc021a30bb44513
nelsonJanusson <nelson.janusson@appliedtechnology.se>
2024-07-14 19:11:44 +0200

    refactor

--- src/main.tsx
+++ src/main.tsx
@@ -1,24 +1,19 @@
-//import React, { StrictMode } from 'react'
 import ReactDOM from "react-dom/client";
 import { RouterProvider, createRouter } from "@tanstack/react-router";
 import { QueryClientProvider, QueryClient } from "@tanstack/react-query";
-// Import the generated route tree
 import { routeTree } from "./routeTree.gen";
 import { StrictMode } from "react";
 
-// Create a new router instance
 const router = createRouter({ routeTree });
 
 const queryClient = new QueryClient();
 
-// Register the router instance for type safety
 declare module "@tanstack/react-router" {
   interface Register {
     router: typeof router;
   }
 }
 
-// Render the app
 const rootElement = document.getElementById("root")!;
 if (!rootElement.innerHTML) {
   const root = ReactDOM.createRoot(rootElement);
