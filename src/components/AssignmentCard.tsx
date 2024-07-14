ebeaa2bbf1bf8ae6d0b4c71ea662cec1645197e7
nelsonJanusson <nelson.janusson@appliedtechnology.se>
2024-07-14 08:13:00 +0200

    random fixes

--- src/components/AssignmentCard.tsx
+++ src/components/AssignmentCard.tsx
@@ -1,7 +1,11 @@
 import axios from "axios";
 import { Assignment, Leg } from "../types";
 import { useMutation } from "@tanstack/react-query";
-
+import { useForm } from "react-hook-form";
+type FormValues = {
+  truck: string;
+  driver: string;
+};
 export function AssignmentCard({
   assignment,
   setAssignments,
@@ -29,19 +33,54 @@ export function AssignmentCard({
     },
   });
 
+  /*const { register, handleSubmit } = useForm<FormValues>();
+  const updateAssignment = useMutation({
+    mutationFn: (e) => {
+      return axios.put(
+        "http://localhost:3000/api/assignment/"+assignment.id+"/"+e.truck+"/"+e.driver,
+        
+      );
+    },
+    onSuccess: (e) => {
+      console.log(e);
+    },
+  });
+  */
+
+  const { register, handleSubmit } = useForm<FormValues>();
+  const onSubmit = (data: FormValues, e) => {
+    console.log("lesgo");
+    axios.put(
+      "http://localhost:3000/api/assignment/" +
+        assignment.id +
+        "/" +
+        data.truck +
+        "/" +
+        data.driver
+    );
+    console.log("lesgo2");
+  };
+  const onError = (errors, e) => console.log(errors, e);
+
   return (
     <div>
       <h4>PickupLocation: {assignment.pickupLocation}</h4>
       <h4>Destination: {assignment.destination}</h4>
       <h4>product: {assignment.product}</h4>
-      <h4>driver: {assignment.driverDto.name}</h4>
-      <h4>truck: {assignment.truckDto.type}</h4>
+      <h4>driver: {assignment.driverDto ? assignment.driverDto.name : ""}</h4>
+      <h4>truck: {assignment.truckDto ? assignment.truckDto.type : ""}</h4>
       {assignment.legs.map((leg: Leg) => (
         <>
           <p>started at {leg.startLocation} </p>
           <p>ended at {leg.endLocation} </p>
         </>
       ))}
+
+      <form onSubmit={handleSubmit(onSubmit, onError)}>
+        <input {...register("truck", { required: true })} />
+        <input {...register("driver", { required: true })} />
+        <input type="submit" />
+      </form>
 
       <button onClick={() => removeAssignment.mutate()}>delete</button>
     </div>
